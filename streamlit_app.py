7cc75ea2714c5ccef6f651b2f9bb4c960c2d16e1
vijay manick <33887114+vijaykite@users.noreply.github.com>
2023-04-19 16:46:20 +0530

    Update streamlit_app.py
    Corrected the syntax error.

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.text('>> Omega 3 & Blueberry Oatmeal 🥗')
 st.text('>> kale, Spinach, & Rocket Smoothie 🥣')
 st.text('>> Hot-Boiled Free-Range egg 🐔')
 st.text('>> Avacado Toast')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
