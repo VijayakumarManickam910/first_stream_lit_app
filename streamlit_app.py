42474ed1de865f640cdb5231f0f7cd99d657a77b
vijay manick <33887114+vijaykite@users.noreply.github.com>
2023-04-19 15:28:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,6 @@
 import streamlit as st
 
 st.title ('My Parents New Healthy Diner')
-st.header(' <p style="font-size:26px; color:blue;">Morning Breakfast</p>',unsafe_allow_html=True)
+st.header('Morning Breakfast')
 st.write('<p style="font-size:26px; color:red;">MONDAY</p>',unsafe_allow_html=True)
-st.write(""" 1) Dosa \n 
-               2) Idly \n 
-               3) Poori \n
-           """)
+st.write('<p style="font-size:26px; color:red;">1) Dosa \n 2) Idly \n 3) Poori \n',unsafe_allow_html=True)
