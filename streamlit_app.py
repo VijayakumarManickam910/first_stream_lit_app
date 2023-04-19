5157ce3656f3fc48dd8c928668b24f90fb5468f2
vijay manick <33887114+vijaykite@users.noreply.github.com>
2023-04-19 16:45:34 +0530

    Update streamlit_app.py
    Added extra text and some changes on previous text.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,12 @@
 import streamlit as st
 
-st.title ('My Parents New Healthy Dinner')
-st.header('Breakfast Menu🥣 🥗 🐔 🥑🍞')
-st.text('   >> Omega 3 & Blueberry Oatmeal 🥗')
-st.text('   >> kale, Spinach, & Rocket Smoothie 🥣')
-st.text('   >> Hot-Boiled Free-Range egg 🐔')
+st.title ("My Mom's New Healthy Dinner")
+st.header("Breakfast Favourites")
+st.text('>> Omega 3 & Blueberry Oatmeal 🥗')
+st.text('>> kale, Spinach, & Rocket Smoothie 🥣')
+st.text('>> Hot-Boiled Free-Range egg 🐔')
+st.text('>> Avacado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
