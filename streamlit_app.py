0b8cdb916a20d3f6c22f378f8914d9198f435109
vijay manick <33887114+vijaykite@users.noreply.github.com>
2023-04-19 16:42:30 +0530

    Update streamlit_app.py
    imported pandas library

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,7 @@ st.header('Breakfast Menu🥣 🥗 🐔 🥑🍞')
 st.text('   >> Omega 3 & Blueberry Oatmeal 🥗')
 st.text('   >> kale, Spinach, & Rocket Smoothie 🥣')
 st.text('   >> Hot-Boiled Free-Range egg 🐔')
+
+import pandas as pd
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)
