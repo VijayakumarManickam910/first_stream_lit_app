f413324f7fbd66288e1d23df70a7515b72db949b
vijay manick <33887114+vijaykite@users.noreply.github.com>
2023-04-19 15:21:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.title ('My Parents New Healthy Diner')
 st.header(' Morning Breakfast')
 st.subheader('MONDAY')
-st.caption("""Dosa \n 
-           Idly \n 
-           Poori""")
+st.caption(""" 1) Dosa \n 
+               2) Idly \n 
+               3) Poori \n
+           """)
