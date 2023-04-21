51c050aa23e16383c2d547d02f66df605a498722
vijay manick <33887114+vijaykite@users.noreply.github.com>
2023-04-21 12:20:03 +0530

    Update streamlit_app.py
    selected fruit name as index.

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,5 +15,4 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 st.multiselect("Pick some fruits:",  list(my_fruit_list.index))
 
-
 st.dataframe(my_fruit_list)
