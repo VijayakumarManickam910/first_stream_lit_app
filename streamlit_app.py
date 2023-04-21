efe6d98c09e20e3e4510f839308b8e93b9904003
vijay manick <33887114+vijaykite@users.noreply.github.com>
2023-04-21 12:15:30 +0530

    Update streamlit_app.py
    Changed multiselect command, "To list".

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,16 +2,17 @@ import streamlit as st
 
 st.title ("My Mom's New Healthy Dinner")
 st.header("Breakfast Favourites")
-st.text('>> Omega 3 & Blueberry Oatmeal 🥗')
-st.text('>> kale, Spinach, & Rocket Smoothie 🥣')
-st.text('>> Hot-Boiled Free-Range egg 🐔')
-st.text('>> Avacado Toast')
+st.text('>> 🥣Omega 3 & Blueberry Oatmeal')
+st.text('>> 🥗kale, Spinach, & Rocket Smoothie ')
+st.text('>> 🐔Hot-Boiled Free-Range egg')
+st.text('>> 🥑Avacado Toast🍞')
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 # Let's put a pick list here so they can pick the fruit they want to include 
-st.multiselect("Pick some fruits:", list(my_fruit_list.index))
+st.multiselect("Pick some fruits:", my_fruit_list['fruit_name'].tolist())
+🥣 🥗 🐔 
 
 
 st.dataframe(my_fruit_list)
