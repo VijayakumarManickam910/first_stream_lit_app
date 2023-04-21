327c9e475ec663a19963bdc3b21102d74c435b33
vijay manick <33887114+vijaykite@users.noreply.github.com>
2023-04-21 12:33:53 +0530

    Update streamlit_app.py
    Got an wrong output, Since displaying dataframe variable had been changed. So it's rectified.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.title ("My Mom's New Healthy Dinner")
 st.header("Breakfast Favourites")
@@ -8,11 +9,10 @@ st.text('>> 🐔Hot-Boiled Free-Range egg')
 st.text('>> 🥑Avacado Toast🍞')
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = st.multiselect("Pick some fruits:",  list(my_fruit_list.index),["Apple", "Strawberries"])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-st.dataframe(my_fruit_list)
+st.dataframe(fruits_to_show)
