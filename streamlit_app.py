96a87ec9d20a76e98ec16e13b93c9e2d18cbe4db
vijay manick <33887114+vijaykite@users.noreply.github.com>
2023-05-10 16:59:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,32 @@
 import streamlit as st
-import pandas as pd
 
 st.title ("My Mom's New Healthy Dinner")
-st.header("Breakfast Favourites")
-st.text('>> 🥣Omega 3 & Blueberry Oatmeal')
-st.text('>> 🥗kale, Spinach, & Rocket Smoothie ')
-st.text('>> 🐔Hot-Boiled Free-Range egg')
-st.text('>> 🥑Avacado Toast🍞')
+
+st.header('Breakfast Favourites')
+st.text('🥣Omega 3 & Blueberry Oatmeal')
+st.text('🥗kale, Spinach, & Rocket Smoothie ')
+st.text('🐔Hot-Boiled Free-Range egg')
+st.text('🥑🍞Avacado Toast')
+
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected = st.multiselect("Pick some fruits:",  list(my_fruit_list.index),["Apple", "Strawberries"])
+fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index),["Apple", "Strawberries"])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-st.dataframe(fruits_to_show)
+
+# Display the table on the page.
+st.dataframe(my_fruit_list)
+
+# New section to display fruityvice api response
+st.header('Fruityvice Fruit Advice')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 import snowflake.connector
-my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchone()
-st.text("The fruit load list contains:")
-st.text(my_data_row)
