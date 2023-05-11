8f35c1bfdba8d43770823b59653feb07b98420ac
vijay manick <33887114+vijaykite@users.noreply.github.com>
2023-05-11 10:56:41 +0530

    Update streamlit_app.py
    Add try catch

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,6 @@ import snowflake.connector
 from urllib.error import URLError
 
 st.title ("My Mom's New Healthy Dinner")
-
 st.header('Breakfast Favourites')
 st.text('🥣Omega 3 & Blueberry Oatmeal')
 st.text('🥗kale, Spinach, & Rocket Smoothie ')
@@ -27,17 +26,18 @@ st.dataframe(fruits_to_show)
 
 # New section to display fruityvice api response
 st.header('Fruityvice Fruit Advice!')
-fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
-st.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = st.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    st.error("Please select a fruit to get information.")
+  
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice) 
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    st.dataframe(fruityvice_normalized)
 
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-st.dataframe(fruityvice_normalized)
-
+expect URLError as e:
+  st.error()
 #don't run anything past here while we troubleshoot
 st.stop()
 
